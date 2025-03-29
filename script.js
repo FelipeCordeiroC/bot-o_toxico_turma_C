395e3eee36b8d41fbcd119a06f6e1340b3dd4ae1
FelipeCordeiroC <felipeclassecordeiro@hotmail.com>
2025-03-29 16:05:47 -0300

    botao_toxico

---
+++ script.js
@@ -0,0 +1,9 @@
+function respostaSim() {
+    
+    alert("Agora somos namorados pra sempre! 💖");
+}
+
+function respostaNao() {
+    
+    document.querySelector("button:nth-child(2)").style.display = "none";
+}
